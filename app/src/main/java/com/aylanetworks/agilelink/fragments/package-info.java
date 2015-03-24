153e033cc3ee1772a1b9207841ec8d1b97ab9cab
Brian King <bri@dufflite.com>
2015-03-24 13:39:28 -0700

    Add package-level JavaDoc files

---
+++ app/src/main/java/com/aylanetworks/agilelink/fragments/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * User interface components of the Agile Link application framework.
+ * <p>
+ * This package contains fragments, dialogs and adapters used to present the user interface
+ * for the Agile Link application.
+ */
+package com.aylanetworks.agilelink.fragments;
