7bed9cc30fbb572dddc1afd9c3591502854b0e2f
Michael Krause <mkrause@cardinalpeak.com>
2015-05-12 09:53:56 -0600

    Keep in sync with mac build script.

--- gradle_scripts/build_script_windows.cmd
+++ gradle_scripts/build_script_windows.cmd
@@ -5,7 +5,8 @@ mkdir libraries
 cd libraries
 git clone https://github.com/AylaNetworks/Android_AylaLibrary.git
 cd Android_AylaLibrary
-git checkout -b master_candidate_merge3 origin/master_candidate_merge3
+: git checkout -b master_candidate_merge3 origin/master_candidate_merge3
+git checkout -b master_candidate origin/master_candidate
 cd ..
 git clone https://github.com/AylaNetworks/Android_AylaZigbeeLibrary.git
 rmdir /s /q Android_AylaLibrary\lib\src\com\aylanetworks\aaml\zigbee
