45ddc3b37f5d05fd161d809f208a6a6fd0d8aed8
Brian King <bri@dufflite.com>
2015-06-01 09:08:35 -0700

    Update gradle scripts to use master branch of library

--- gradle_scripts/build_script_mac.sh
+++ gradle_scripts/build_script_mac.sh
@@ -9,7 +9,7 @@ git clone https://github.com/AylaNetworks/Android_AylaZigbeeLibrary.git
 
 export ZIGBEE_PATH=$PWD
 cd Android_AylaLibrary
-git checkout -b master_candidate origin/master_candidate
+git checkout master
 #git checkout -b master_candidate_merge3 origin/master_candidate_merge3
 rm -rf lib/src/com/aylanetworks/aaml/zigbee
 ln -s $ZIGBEE_PATH/Android_AylaZigbeeLibrary/zigbee lib/src/com/aylanetworks/aaml/zigbee
