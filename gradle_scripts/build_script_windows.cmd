ccc5a8e3b988e31e3cf0d012f96ddbc134f6084c
Michael Krause <michaeldouglaskrause@gmail.com>
2015-08-21 07:55:43 -0600

    Make sure to use develop branch of ayla lib

--- gradle_scripts/build_script_windows.cmd
+++ gradle_scripts/build_script_windows.cmd
@@ -3,12 +3,8 @@ cd ..
 rmdir /s /q libraries
 mkdir libraries
 cd libraries
-git clone https://github.com/AylaNetworks/Android_AylaLibrary.git
-cd Android_AylaLibrary
-: git checkout -b master_candidate_merge3 origin/master_candidate_merge3
-git checkout master
-cd ..
-git clone https://github.com/AylaNetworks/Android_AylaZigbeeLibrary.git
+git clone -b develop https://github.com/AylaNetworks/Android_AylaLibrary.git
+git clone -b develop https://github.com/AylaNetworks/Android_AylaZigbeeLibrary.git
 rmdir /s /q Android_AylaLibrary\lib\src\com\aylanetworks\aaml\zigbee
 xcopy Android_AylaZigbeeLibrary\zigbee Android_AylaLibrary\lib\src\com\aylanetworks\aaml\zigbee\ /s /e /y
 rmdir /s /q Android_AylaZigbeeLibrary
