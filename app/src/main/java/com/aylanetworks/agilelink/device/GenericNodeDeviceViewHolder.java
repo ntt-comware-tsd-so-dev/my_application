85894c49b58360b256e748921350b8c10d0871e9
aylanetworks-di <di@aylanetworks.com>
2016-03-29 16:25:23 -0700

    Add GenericNodeDeviceViewHolder class.

---
+++ app/src/main/java/com/aylanetworks/agilelink/device/GenericNodeDeviceViewHolder.java
@@ -0,0 +1,24 @@
+package com.aylanetworks.agilelink.device;
+
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.aylanetworks.agilelink.R;
+import com.aylanetworks.agilelink.framework.GenericDeviceViewHolder;
+
+/**
+ * AMAP4.x
+ * <p/>
+ * Copyright 2016 Ayla Networks Inc, all rights reserved
+ */
+public class GenericNodeDeviceViewHolder extends GenericDeviceViewHolder {
+    public ImageButton _switchButton;
+
+    public GenericNodeDeviceViewHolder(View view) {
+        super(view);
+        _switchButton = (ImageButton)view.findViewById(R.id.power_button);
+    }
+}// end of GenericNodeDeviceViewHolder class
+
+
+
