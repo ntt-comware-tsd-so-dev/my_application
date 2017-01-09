aeb2732c642e9b4e74d46d495307916ad8fcc49a
https://github.com/AylaNetworks/Android_AylaLibrary.git <raghu@aylanetworks.com>
2017-01-09 12:09:33 -0800

    Remove unused import

--- app/src/main/java/com/aylanetworks/agilelink/beacon/BeaconsListFragment.java
+++ app/src/main/java/com/aylanetworks/agilelink/beacon/BeaconsListFragment.java
@@ -22,7 +22,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
