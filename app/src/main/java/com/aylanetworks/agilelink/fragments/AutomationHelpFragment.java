329a6d009d280831bce643c8f1a53724d8f080fd
https://github.com/AylaNetworks/Android_AylaLibrary.git <raghu@aylanetworks.com>
2017-01-12 13:04:45 -0800

    Added spanish html page

--- app/src/main/java/com/aylanetworks/agilelink/fragments/AutomationHelpFragment.java
+++ app/src/main/java/com/aylanetworks/agilelink/fragments/AutomationHelpFragment.java
@@ -9,17 +9,27 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 
 import com.aylanetworks.agilelink.R;
+
+import java.util.Locale;
+
 /*
  * AMAP_Android
  *
  * Copyright 2016 Ayla Networks, all rights reserved
  */
 public class AutomationHelpFragment extends Fragment {
+    private final String LANG_ES ="es";
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.automation_help, container, false);
         WebView webView = (WebView) root.findViewById(R.id.webview);
-        webView.loadUrl("file:///android_res/raw/automation_help.htm");
+        String fileURL="file:///android_res/raw/automation_help.htm";
+
+        if(LANG_ES.equals(Locale.getDefault().getLanguage())){
+            fileURL  ="file:///android_res/raw/automation_help_es.htm";
+        }
+
+        webView.loadUrl(fileURL);
         return root;
     }
     public static AutomationHelpFragment newInstance() {
