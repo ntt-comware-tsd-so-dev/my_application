ffd696b3c25a5d0b06afc5aa1d245db87c237797
Raji Pillay <raji@aylanetworks.com>
2017-01-18 14:11:23 -0800

    Remove commented code

--- app/src/main/java/com/aylanetworks/agilelink/device/AylaEVBDevice.java
+++ app/src/main/java/com/aylanetworks/agilelink/device/AylaEVBDevice.java
@@ -162,10 +162,6 @@ public class AylaEVBDevice extends GenericDevice implements View.OnClickListener
             } else {
                 setBlueLED(!isBlueLEDOn(), button);
             }
-
-            // Update the image view to show the transient state
-            /*button.setImageDrawable(v.getContext().getResources().getDrawable(R.drawable
-                    .dpending));*/
         } else {
             Toast.makeText(MainActivity.getInstance(), R.string.offline_no_functionality,
                     Toast.LENGTH_SHORT).show();
